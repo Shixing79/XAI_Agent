788ecb2c4e02a7bd56f10b758f582357911cb6c7
Tom-C79 <cheng.shixing7799@gmail.com>
2025-06-20 17:56:12 +0100

    small change

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from actions import known_actions
 
 if __name__ == "__main__":
     agent = Agent(system_prompt=open("system_prompt.txt", "r").read(), max_turns=5, known_actions=known_actions)
-    print("What is your question for the Forecast Genie?\nType 'q', 'quit' or 'exit' to end the conversation.")    
+    print("What is your question for the Agent?\nType 'q', 'quit' or 'exit' to end the conversation.")    
     while True:
         user_question = input("You: ")
         if user_question.strip().lower() in ['q', "quit", "exit"]:
